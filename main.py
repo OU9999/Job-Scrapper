9b3714af3a8bfb4e3d618e5d81f16e08875900b9
omh232323 <omh232323@gmail.com>
2022-11-15 19:36:49 +0900

    ok
    ok

--- main.py
+++ main.py
@@ -1,38 +1,42 @@
-from flask import Flask, render_template,request,redirect,send_file
+from flask import Flask, render_template, request, redirect, send_file
 from extractors.wwr import extract_wwr_jobs
 from extractors.remoteok import extract_remote_jobs
 from file import save_to_file
 
 app = Flask("JobScrapper")
 
-db={}
+db = {}
+
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+  return render_template("home.html")
+
 
 @app.route("/search")
 def hello():
-    keyword = request.args.get("keyword")
-    if keyword == None :
-        return redirect("/")
-    if keyword in db:
-        jobs = db[keyword]
-    else :
-        remote = extract_remote_jobs(keyword)
-        wwr = extract_wwr_jobs(keyword)
-        jobs = remote+wwr
-        db[keyword]=jobs
-    return render_template("search.html",keyword=keyword,jobs=jobs)
+  keyword = request.args.get("keyword")
+  if keyword == None:
+    return redirect("/")
+  if keyword in db:
+    jobs = db[keyword]
+  else:
+    remote = extract_remote_jobs(keyword)
+    wwr = extract_wwr_jobs(keyword)
+    jobs = remote + wwr
+    db[keyword] = jobs
+  return render_template("search.html", keyword=keyword, jobs=jobs)
+
 
 @app.route("/export")
-def hi() :
-    keyword = request.args.get("keyword")
-    if keyword == None :
-        return redirect("/")
-    if keyword not in db:
-        return redirect(f"/search?keyword={keyword}")
-    save_to_file(keyword,db[keyword])
-    return send_file(f"{keyword}.csv", as_attachment=True)
+def hi():
+  keyword = request.args.get("keyword")
+  if keyword == None:
+    return redirect("/")
+  if keyword not in db:
+    return redirect(f"/search?keyword={keyword}")
+  save_to_file(keyword, db[keyword])
+  return send_file(f"{keyword}.csv", as_attachment=True)
 
-app.run("127.0.0.1")
+
+app.run("0.0.0.0")
